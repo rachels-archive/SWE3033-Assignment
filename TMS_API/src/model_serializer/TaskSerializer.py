a44aeca96d117b6e8b3d37600da078ec7280db76
Ryan Chan <ryanchanjy@outlook.com>
2024-05-22 15:25:47 +0800

    install packages

--- TMS_API/src/model_serializer/TaskSerializer.py
+++ TMS_API/src/model_serializer/TaskSerializer.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
-
 from src.model import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
     model = Task
-    fields = ['id', 'task_id', 'task_description', 'task_deadline', 'task_priority', 'task_status','is_active']
+    fields = ['id', 'task_id', 'task_description', 'task_deadline', 'task_priority', 'task_status', 'is_active']
