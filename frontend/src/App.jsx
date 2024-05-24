d541c5a224102af82d9021ca7d4cffca370ba614
rachels-archive <rachelhui@protonmail.com>
2024-05-24 21:48:27 +0800

    Initialise React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='container-fluid full-screen'>
+      <nav>
+        <h1>Task Manager</h1>
+     </nav>
+    </div>
+  )
+}
+
+export default App
