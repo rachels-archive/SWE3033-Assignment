eaa64c958cc932f219f82afed659dc5a86b96ef8
rachels-archive <rachelhui@protonmail.com>
2024-05-25 14:41:42 +0800

    Add create, update and delete task views

--- TMS_API/tms_apis/views.py
+++ TMS_API/tms_apis/views.py
@@ -1,14 +1,46 @@
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from rest_framework import status
 from tms_apis.models import Task
 from tms_apis.serializers import TaskSerializer
-
+from uuid import uuid4
 
 class TaskView(APIView):
     def get(self, request, format=None):
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data)
+    
+class CreateTaskView(APIView):
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UpdateTaskView(APIView):
+    def put(self, request, pk):
+        try:
+            task = Task.objects.get(pk=pk)
+        except Task.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteTaskView(APIView):
+    def delete(self, request, pk):
+        try:
+            task = Task.objects.get(pk=pk)
+            task.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Task.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
