fb7c40af8b3c1211aa89300277a253e5210b88ba
rachels-archive <rachelhui@protonmail.com>
2024-05-25 14:41:08 +0800

    Update task serialiser

--- TMS_API/tms_apis/serializers.py
+++ TMS_API/tms_apis/serializers.py
@@ -2,8 +2,12 @@ from rest_framework import serializers
 
 from tms_apis.models import Task
 
-
 class TaskSerializer(serializers.ModelSerializer):
+    task_id = serializers.SerializerMethodField()
+
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = [ 'id','task_name', 'task_description', 'task_deadline', 'task_priority', 'task_status', 'task_id', 'is_active']
+
+    def get_task_id(self, obj):
+        return f"T_{obj.id}"
