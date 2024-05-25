e121f7926309349a0435b1c52dfbacb8a0683884
rachels-archive <rachelhui@protonmail.com>
2024-05-25 14:40:29 +0800

    Update task model

--- TMS_API/tms_apis/models.py
+++ TMS_API/tms_apis/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+import uuid
 
 class Task(models.Model):
     id = models.AutoField(primary_key=True, editable=False, unique=True)
-    task_id = models.CharField(max_length=8, null=False, unique=True)
+    task_id = models.CharField(max_length=8, null=False, unique=True, default=lambda: f"T_{uuid.uuid4().hex[:6].upper()}")
     task_name = models.CharField(max_length=128, null=False)
-    task_description = models.TextField(max_length=255, null=True)
+    task_description = models.TextField(max_length=255, null=True, blank=True)
     task_deadline = models.TextField(max_length=32, null=True)
     task_priority = models.IntegerField(null=False)
     task_status = models.IntegerField(null=False)
